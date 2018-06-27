620d2248fb936b4d2df6287b9a89eda2c2f05fa0
andrzej <andrzej.acc@gmail.com>
2018-06-27 13:55:27 +0200

    Add GET /users/me and authenication middleware

--- server/models/user.js
+++ server/models/user.js
@@ -43,11 +43,26 @@ UserSchema.methods.generateAuthToken = function () {
     var user = this;
     var access = 'auth';
     var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
-    console.log('Token is ', token);
     user.tokens = user.tokens.concat([{access, token}]);
-    console.log('Tokens are ', user.tokens);
     return user.save().then(() => {
         return token;
+    });
+};
+
+UserSchema.statics.findByToken = function (token) {
+    var User = this;
+    var decoded;
+
+    try {
+        decoded = jwt.verify(token, 'abc123');
+    } catch (e) {
+        return Promise.reject();
+    }
+
+    return User.findOne({
+        '_id': decoded._id,
+        'tokens.token': token,
+        'tokens.access': 'auth'
     });
 };
 
